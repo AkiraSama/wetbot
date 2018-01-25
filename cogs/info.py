39755bee8624f4eedd524e60edbba864894b08c5
akirasama <akirasama@users.noreply.github.com>
2018-01-25 15:19:53 +0000

    reimplement !define command
    temporary single definition since the new API is packed with information
    and I don't have the gumption to parse multiple definitions atm

--- cogs/info.py
+++ cogs/info.py
@@ -11,6 +11,7 @@ log.setLevel(logging.DEBUG)
 
 WIKIPEDIA_URL = 'https://en.wikipedia.org/w/api.php'
 UD_URL = 'http://api.urbandictionary.com/v0/define?term={}'
+OXFORD_URL = 'https://od-api.oxforddictionaries.com/api/v1'
 
 def json_format(response):
     return '```json\n{}```'.format(
@@ -101,6 +102,57 @@ class InfoCog(object):
                     out = json_format(response)
 
         await ctx.send(out)
+
+    @commands.command(aliases=('d',))
+    async def define(self, ctx, *, query):
+        """mommy fixed it"""
+        app_id = self.bot.config.get('oxford_dictionaries', 'app_id', '')
+        app_key = self.bot.config.get('oxford_dictionaries', 'app_key', '')
+        if not app_id or not app_key:
+            await ctx.send("configuration incomplete")
+            return
+        headers = {
+            'app_id': app_id,
+            'app_key': app_key
+        }
+
+        word_id = None
+        async with self.session.get(
+            OXFORD_URL + '/search/en?q=' + urllib.parse.quote(query),
+            headers=headers,
+            timeout=5
+        ) as resp:
+            if resp.status == 200:
+                response = await resp.json()
+                if response['metadata']['total'] > 0:
+                    word_id = response['results'][0]['id']
+                else:
+                    await ctx.send("I got nothin ¯\_(ツ)_/¯")
+                    return
+
+        async with self.session.get(
+            OXFORD_URL + '/entries/en/' + word_id,
+            headers=headers,
+            timeout=5
+        ) as resp:
+            if resp.status == 200:
+                result = (await resp.json())['results'][0]
+
+        entry = result['lexicalEntries'][0]
+        d = (
+            '```\n{word}\n/{pronunciation}/, '
+            '{category}\n{definition}```'
+        ).format(
+            word=entry['text'],
+            pronunciation='/ ᴏʀ /'.join(
+                p['phoneticSpelling']
+                for p
+                in entry['pronunciations']),
+            category=entry['lexicalCategory'].lower(),
+            definition=entry['entries'][0]['senses'][0]['definitions'][0]
+        )
+
+        await ctx.send(d)
 
 def setup(bot):
     log.info("adding InfoCog to bot")
