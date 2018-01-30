a78d552d3d39c935bd5e9d388c264330e2554566
akirasama <akirasama@users.noreply.github.com>
2018-01-30 02:49:35 +0000

    added definition cycling

--- cogs/info.py
+++ cogs/info.py
@@ -13,6 +13,11 @@ WIKIPEDIA_URL = 'https://en.wikipedia.org/w/api.php'
 UD_URL = 'http://api.urbandictionary.com/v0/define?term={}'
 OXFORD_URL = 'https://od-api.oxforddictionaries.com/api/v1'
 
+DEF_TEMPLATE = (
+    '```\n{word}\n/{pronunciation}/, '
+    '{category}\n{definition}```'
+)
+
 def json_format(response):
     return '```json\n{}```'.format(
         json.dumps(response, indent='  '))
@@ -21,6 +26,7 @@ class InfoCog(object):
     def __init__(self, bot):
         self.bot = bot
         self.session = bot.http._session
+        self.active_definitions = {}
 
     async def _wiki_get_pageid(self, query):
         query = urllib.parse.quote_plus(query)
@@ -68,7 +74,7 @@ class InfoCog(object):
                         '&format=json'
                         '&pageids={}'
                         '&inprop=url')
-        
+
         async with self.session.get(
             WIKIPEDIA_URL + query_string.format(pageid),
             timeout=5
@@ -103,9 +109,56 @@ class InfoCog(object):
 
         await ctx.send(out)
 
+    def iterate_definitions(self, response):
+        for lexical_entry in response['lexicalEntries']:
+            word = lexical_entry['text']
+            pronunciation = '/ ᴏʀ /'.join(
+                p['phoneticSpelling']
+                for p
+                in lexical_entry['pronunciations']
+            )
+            category = lexical_entry['lexicalCategory'].lower()
+            for entry in lexical_entry['entries']:
+                for sense in entry['senses']:
+                    if 'definitions' not in sense:
+                        continue
+                    for definition in sense['definitions']:
+                        yield {
+                            'word': word,
+                            'pronunciation': pronunciation,
+                            'category': category,
+                            'definition': definition
+                        }
+                    if 'subsenses' not in sense:
+                        continue
+                    for subsense in sense['subsenses']:
+                        for definition in subsense['definitions']:
+                            yield {
+                                'word': word,
+                                'pronunciation': pronunciation,
+                                'category': category,
+                                'definition': definition
+                            }
+
+
     @commands.command(aliases=('d',))
-    async def define(self, ctx, *, query):
+    async def define(self, ctx, *, query=None):
         """mommy fixed it"""
+        if not query:
+            try:
+                if ctx.channel.id in self.active_definitions:
+                    await ctx.send(DEF_TEMPLATE.format(
+                        **next(self.active_definitions[
+                            ctx.channel.id
+                        ])))
+                    return
+                else:
+                    await ctx.send("no active definitions!")
+            except StopIteration:
+                await ctx.send("ran outta definitions, friend")
+                del self.active_definitions[ctx.channel.id]
+                return
+
         app_id = self.bot.config.get('oxford_dictionaries', 'app_id', '')
         app_key = self.bot.config.get('oxford_dictionaries', 'app_key', '')
         if not app_id or not app_key:
@@ -138,21 +191,13 @@ class InfoCog(object):
             if resp.status == 200:
                 result = (await resp.json())['results'][0]
 
-        entry = result['lexicalEntries'][0]
-        d = (
-            '```\n{word}\n/{pronunciation}/, '
-            '{category}\n{definition}```'
-        ).format(
-            word=entry['text'],
-            pronunciation='/ ᴏʀ /'.join(
-                p['phoneticSpelling']
-                for p
-                in entry['pronunciations']),
-            category=entry['lexicalCategory'].lower(),
-            definition=entry['entries'][0]['senses'][0]['definitions'][0]
+        self.active_definitions[ctx.channel.id] = (
+            self.iterate_definitions(result)
         )
 
-        await ctx.send(d)
+        await ctx.send(DEF_TEMPLATE.format(
+            **next(self.active_definitions[ctx.channel.id])
+        ))
 
 def setup(bot):
     log.info("adding InfoCog to bot")
